8b40538fbebf35a0b7434f34e8526aece433d08e
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-27 00:13:07 +0100

    Add TODO (Case.java)

--- Labyrinthe/src/Case.java
+++ Labyrinthe/src/Case.java
@@ -1,5 +1,6 @@
-
+/* TODO Javadoc. */
+/* TODO Package. */
 public interface Case {
-	
-	public String afficher();
+    /* TODO Les méthodes d'une interface sont forcement "public" donc vous n'etes pas obligé de l'indiquer. */
+    public String afficher();
 }
