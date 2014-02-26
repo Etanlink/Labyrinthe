0acdee38a5f54a0585b241afdc03ec01a1d7707a
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-27 00:12:59 +0100

    Add TODO (Application.java)

--- Labyrinthe/src/Application.java
+++ Labyrinthe/src/Application.java
@@ -1,11 +1,8 @@
-
+/* TODO Javadoc. */
+/* TODO Package. */
 public class Application {
-
-	public static void main(String[] args) {
-		
-		Jeu j = new Jeu(); // Initialisation de la partie
-		j.jouer(); // Premier tour de jeu
-		
-	}
-
+    public static void main(String[] args) {
+        Jeu j = new Jeu(); // Initialisation de la partie
+        j.jouer(); // Premier tour de jeu
+    }
 }
